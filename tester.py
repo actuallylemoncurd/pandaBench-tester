f56fbd41d1ea8e36554319ca9a7e08af51244c7d
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2023-02-16 18:31:22 -0500

    add set cruise and cancel

--- tester.py
+++ tester.py
@@ -15,12 +15,24 @@ writer = csv.writer(f)
 while True:
     p.can_clear(0xFFFF)
     p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-    # GRA_Neu
+    # GRA_Neu SETCRUISE
     p.can_send(0x38a, "0d010c00", 0)
     p.can_send(0x38a, "2d012c00", 0)
     # Motor_2
     p.can_send(0x288, "327f300400536600", 0)
     p.can_send(0x288, "327f200400536600", 0)
+    p.can_send(0x38a, "2d012c04", 0)
+    # Bremse_3
+    p.can_send(0x4A0, "b804ba04aa04b604", 0)
+    p.can_send(0x4A0, "60045e044e045c04", 0)
+
+    # GRA_Neu LEVEL CANCEL
+    p.can_send(0x38a, "0d010c00", 0)
+    p.can_send(0x38a, "2d012c00", 0)
+    # Motor_2
+    p.can_send(0x288, "327f300400536600", 0)
+    p.can_send(0x288, "327f200400536600", 0)
+    p.can_send(0x38a, "2d112c00", 0)
     # Bremse_3
     p.can_send(0x4A0, "b804ba04aa04b604", 0)
     p.can_send(0x4A0, "60045e044e045c04", 0)
