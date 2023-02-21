e3b71eeaf34c060993747503fcaab790623c1c2e
Curtis Jenkins <cuj22001@sa.uconn.edu>
2023-02-21 14:55:19 -0500

    version 2 of test script

--- tester.py
+++ tester.py
@@ -3,8 +3,8 @@ import csv
 from panda import Panda
 p = Panda()
 
-f = open('benchtestResults.csv', 'w')
-writer = csv.writer(f)
+w = open('benchtestResults.csv', 'w')
+writer = csv.writer(w)
 
 # addr, data, bus
 # can 0 is our feed data onto CAR PT can
@@ -12,36 +12,21 @@ writer = csv.writer(f)
 # can 2 is our extended receive can
 # (0, 1, 2) for RX, (128, 129, 130) for TX
 
-while True:
-    p.can_clear(0xFFFF)
-    p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-    # GRA_Neu SETCRUISE
-    p.can_send(0x38a, "0d010c00", 0)
-    p.can_send(0x38a, "2d012c00", 0)
-    # Motor_2
-    p.can_send(0x288, "327f300400536600", 0)
-    p.can_send(0x288, "327f200400536600", 0)
-    p.can_send(0x38a, "2d012c04", 0)
-    # Bremse_3
-    p.can_send(0x4A0, "b804ba04aa04b604", 0)
-    p.can_send(0x4A0, "60045e044e045c04", 0)
-
-    # GRA_Neu LEVEL CANCEL
-    p.can_send(0x38a, "0d010c00", 0)
-    p.can_send(0x38a, "2d012c00", 0)
-    # Motor_2
-    p.can_send(0x288, "327f300400536600", 0)
-    p.can_send(0x288, "327f200400536600", 0)
-    p.can_send(0x38a, "2d112c00", 0)
-    # Bremse_3
-    p.can_send(0x4A0, "b804ba04aa04b604", 0)
-    p.can_send(0x4A0, "60045e044e045c04", 0)
-
-    # junk test msg's
-    p.can_send(0x4A6, "b804ba04aa04b604", 0)
-    p.can_send(0x4A6, "60045e044e045c04", 0)
-
-    can_recv = p.can_recv()
-    time.sleep(0.01)
-    if len(can_recv) > 0:
-        writer.writerow(can_recv)
+with open('ocelot_j533-testData.csv', 'r') as testData:
+    reader = csv.reader(testData)
+    for row in reader:
+        #p.can_clear(0xFFFF)
+        #p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+        addr = int(row[0])
+        addr = hex(addr)
+        dat = str(row[1])
+        if int(row[0]) == 1386:
+            p.can_send(addr, '"'+dat+'"', 2)
+            #print(addr, '"'+dat+'"', 2)
+        else:
+            p.can_send(addr, '"'+dat+'"', 0)
+            #print(addr, '"'+dat+'"', 0)
+        can_recv = p.can_recv()
+        #time.sleep(0.01)
+        if len(can_recv) > 0:
+           writer.writerow(can_recv)
