4418c2429dc9f4892f3a3b6cb687f30cf5b8a8f3
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2023-02-25 16:49:46 -0500

    add pass fail

--- functionVerifier.py
+++ functionVerifier.py
@@ -1,10 +1,18 @@
-# todo: now that all the signals are here, add some conditionals to parse their outputs, and give a pass fail for module functionality
+# todo: now that all the signals are here, add some conditionals to parse their outputs
+#       give a pass fail for module functionality
 
 import csv
 
 GRA_Neu = 0x38A
 mACC_GRA_Anziege = 0x56a
 mACC_System = 0x368
+
+# init all signal counters
+kodierCounter = 0
+senderCounter = 0
+staACCCounter = 0
+accSystemCounter = 0
+ststinfoCounter = 0
 
 with open('benchtestResults.csv', 'r') as resultData:
     reader = csv.reader(resultData)
@@ -13,7 +21,8 @@ with open('benchtestResults.csv', 'r') as resultData:
             addr = hex(int(row[0]))
             dat = bin(int(row[1], 16)).ljust(66, '0')
             if addr == hex(GRA_Neu):
-                # Message from cruise control stalk, most important to make sure the bitflipping is correct!
+                # Message from cruise control stalk, most important to make
+                # sure the bitflipping is correct!
                 GRA_Kodierinfo = bin((int(dat, 2) >> 48) & int(1))[2]
                 GRA_Sender = bin((int(dat, 2) >> 44) & int(3)).ljust(4, '0')[2:4]
             if addr == hex(mACC_GRA_Anziege):
@@ -26,6 +35,7 @@ with open('benchtestResults.csv', 'r') as resultData:
                 ACA_gemZeitl = bin((int(dat, 2) >> 20) & int(15)).ljust(6, '0')[2:6]
                 ACA_Aend_Zeitluecke = bin((int(dat, 2) >> 5) & int(1))[2]
             if addr == hex(mACC_System):
+                accSystemCounter = accSystemCounter + 1
                 # Message entirely from ocelot
                 ACS_Zaehler = bin((int(dat, 2) >> 52) & int(15)).ljust(6, '0')[2:6]
                 ACS_Sta_ADR = bin((int(dat, 2) >> 50) & int(3)).ljust(4, '0')[2:4]
@@ -36,5 +46,32 @@ with open('benchtestResults.csv', 'r') as resultData:
                 ACS_Sollbeschl = bin((int(dat, 2) >> 29) & int(2045)).ljust(13, '0')[2:13]
                 ACS_zul_Regelabw = bin((int(dat, 2) >> 20) & int(255)).ljust(10, '0')[2:10]
                 ACS_max_AendGrad = bin((int(dat, 2) >> 8) & int(255)).ljust(10, '0')[2:10]
+            # increment signal incorrect counters
+            if str(GRA_Kodierinfo) != '1': kodierCounter = kodierCounter + 1
+            if str(GRA_Sender) != '01': senderCounter = senderCounter + 1
+            if str(ACA_StaACC) == '000': staACCCounter = staACCCounter + 1
+            if str(ACS_StSt_Info) == '1': ststinfoCounter = ststinfoCounter + 1
         except Exception as e:
             print("an exception occured" + str(e))
+    # if matrix for pass / fail
+    # 1000 = 10 seconds of collective faults (100hz)
+    if kodierCounter >= 1000:
+        print("GRA_Kodierinfo was wrong "+f'{kodierCounter}'+" times")
+    else:
+        print("GRA_Kodierinfo PASS")
+    if senderCounter >= 1000:
+        print("GRA_Sender was wrong "+f'{senderCounter}'+" times")
+    else:
+        print("GRA_Sender PASS")
+    if staACCCounter >= 1000:
+        print("mACC_GRA_Anziege was wrong "+f'{staACCCounter}'+" times")
+    else:
+        print("mACC_GRA_Anziege PASS")
+    if accSystemCounter <= 1000:
+        print("mACC_System FAIL, MSG not present")
+        if ststinfoCounter <= 1000:
+            print("ACS_StSt_Info was wrong "+f'{ststinfoCounter}'+" times")
+    else:
+        print("mACC_System PRESENT")
+        if ststinfoCounter >= 1000:
+            print("ACS_StSt_Info PASS")
